bbfaef3f21955e3413c3b75a65470167316a6228
shall <hall.steven.g@gmail.com>
2014-04-02 16:27:54 -0700

    remove render from resize function

--- js/viz.js
+++ js/viz.js
@@ -199,7 +199,6 @@
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    VIZ.render();
   }
   window.VIZ = VIZ;
 }())
